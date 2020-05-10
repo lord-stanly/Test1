ab84589f1e5778e1c4a65863b3600d06692434e0
Lord-Stanly <fizikello@mail.ru>
2020-05-10 14:58:42 +0300

    start

--- app/src/main/java/ru/mail/fizikello/test1/MainActivity.java
+++ app/src/main/java/ru/mail/fizikello/test1/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -11,6 +12,7 @@ import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,7 +22,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
+private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +30,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open,
@@ -52,28 +54,29 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = menuItem.getItemId();
         if(id == R.id.nav_home)
         {
-
+            Toast.makeText(this, "Nav hom pressed", Toast.LENGTH_SHORT).show();
         }
         else if(id == R.id.nav_gallery)
         {
-
+            Toast.makeText(this, "nav gallery pressed", Toast.LENGTH_SHORT).show();
         }
         else if(id == R.id.nav_slideshow)
         {
-
+            Toast.makeText(this, "pressed", Toast.LENGTH_SHORT).show();
         }
         else if(id == R.id.nav_tools)
         {
-
+            Toast.makeText(this, "pressed", Toast.LENGTH_SHORT).show();
         }
         else if(id == R.id.nav_shape)
         {
-
+            Toast.makeText(this, "pressed", Toast.LENGTH_SHORT).show();
         }
         else if(id == R.id.nav_send)
         {
-
+            Toast.makeText(this, "pressed", Toast.LENGTH_SHORT).show();
         }
+        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 }
