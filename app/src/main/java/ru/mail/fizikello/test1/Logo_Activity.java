ca817fbeb0724a85fb7965b10f83c881b1c98950
Lord-Stanly <fizikello@mail.ru>
2020-05-16 14:37:15 +0300

    5.0 добавлена анимация логотипа

--- app/src/main/java/ru/mail/fizikello/test1/Logo_Activity.java
+++ app/src/main/java/ru/mail/fizikello/test1/Logo_Activity.java
@@ -4,14 +4,36 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
 public class Logo_Activity extends Activity {
+    private Animation logoAnim, buttonLogoAnim;
+    private Button bAnim;
+    private ImageView logoImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.logo_activity);
+        init();
+
+    }
+    private void init(){
+        // Загружаем анимации в переменные
+        logoAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.logo_anim);
+        buttonLogoAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.button_anim);
+
+
+        logoImage = findViewById(R.id.logoView) ;
+        bAnim = findViewById(R.id.buttonAnim);
+        //Запускаем анимацию
+        logoImage.startAnimation(logoAnim);
+        bAnim.startAnimation(buttonLogoAnim);
 
     }
 
